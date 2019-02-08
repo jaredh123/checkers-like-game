b93a43b1a9fd7935ccd425b27e5b38052a3cf715
jaredh123 <46456502+jaredh123@users.noreply.github.com>
2019-02-07 16:02:42 -0800

    Update group changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,303 +1,132 @@
+var playerTurn = 0;
+var individualMovement = 0;
+var startPosition = [];
+var startPositionID;
+var horizontalStartPosition;
+var veritcalStartPosition;
+var endingPosition = [];
+var endingPositionID;
+var horizontalEndingPosition;
+var veritcalEndingPosition;
+var currentPiece = [];
+var playerColor = 1;
+var emptySpace = 0;
+var redPiece = 1;
+var blackPiece = 2;
+var redScore = 0;
+var blackScore = 0;
 
-
-
+var A = [1, 0, 1, 0, 0, 0, 2, 0];
+var B = [0, 1, 0, 0, 0, 2, 0, 2];
+var C = [1, 0, 1, 0, 0, 0, 2, 0];
+var D = [0, 1, 0, 0, 0, 2, 0, 2];
+var E = [1, 0, 1, 0, 0, 0, 2, 0];
+var F = [0, 1, 0, 0, 0, 2, 0, 2];
+var G = [1, 0, 1, 0, 0, 0, 2, 0];
+var H = [0, 1, 0, 0, 0, 2, 0, 2];
+var letters = ["A", "B", "C", "D", "E", "F", "G", "H"]
 
 $(document).ready(function() {
-
-  // $("#position").text("gg");
-  var position = 11
-
-  var variableIdName = function() {
-    if (position == 11) {
-      return "A1";
-    }
-    else if (position == 12) {
-      return "A2";
-    }
-    else if (position == 13) {
-      return "A3";
-    }
-    else if (position == 14) {
-      return "A4";
-    }
-    else if (position == 15) {
-      return "A5";
-    }
-    else if (position == 16) {
-      return "A6";
-    }
-    else if (position == 17) {
-      return "A7";
-    }
-    else if (position == 18) {
-      return "A8";
-    }
-
-    else if (position == 21) {
-      return "B1";
-    }
-    else if (position == 22) {
-      return "B2";
-    }
-    else if (position == 23) {
-      return "B3";
-    }
-    else if (position == 24) {
-      return "B4";
-    }
-    else if (position == 25) {
-      return "B5";
-    }
-    else if (position == 26) {
-      return "B6";
-    }
-    else if (position == 27) {
-      return "B7";
-    }
-    else if (position == 28) {
-      return "B8";
-    }
-
-    else if (position == 31) {
-      return "C1";
-    }
-    else if (position == 32) {
-      return "C2";
-    }
-    else if (position == 33) {
-      return "C3";
-    }
-    else if (position == 34) {
-      return "C4";
-    }
-    else if (position == 35) {
-      return "C5";
-    }
-    else if (position == 36) {
-      return "C6";
-    }
-    else if (position == 37) {
-      return "C7";
-    }
-    else if (position == 38) {
-      return "C8";
-    }
-
-    else if (position == 41) {
-      return "D1";
-    }
-    else if (position == 42) {
-      return "D2";
-    }
-    else if (position == 43) {
-      return "D3";
-    }
-    else if (position == 44) {
-      return "D4";
-    }
-    else if (position == 45) {
-      return "D5";
-    }
-    else if (position == 46) {
-      return "D6";
-    }
-    else if (position == 47) {
-      return "D7";
-    }
-    else if (position == 48) {
-      return "D8";
-    }
-
-    else if (position == 51) {
-      return "E1";
-    }
-    else if (position == 52) {
-      return "E2";
-    }
-    else if (position == 53) {
-      return "E3";
-    }
-    else if (position == 54) {
-      return "E4";
-    }
-    else if (position == 55) {
-      return "E5";
-    }
-    else if (position == 56) {
-      return "E6";
-    }
-    else if (position == 57) {
-      return "E7";
-    }
-    else if (position == 58) {
-      return "E8";
-    }
-
-    else if (position == 61) {
-      return "F1";
-    }
-    else if (position == 62) {
-      return "F2";
-    }
-    else if (position == 63) {
-      return "F3";
-    }
-    else if (position == 64) {
-      return "F4";
-    }
-    else if (position == 65) {
-      return "F5";
-    }
-    else if (position == 66) {
-      return "F6";
-    }
-    else if (position == 67) {
-      return "F7";
-    }
-    else if (position == 68) {
-      return "F8";
-    }
-
-    else if (position == 71) {
-      return "G1";
-    }
-    else if (position == 72) {
-      return "G2";
-    }
-    else if (position == 73) {
-      return "G3";
-    }
-    else if (position == 74) {
-      return "G4";
-    }
-    else if (position == 75) {
-      return "G5";
-    }
-    else if (position == 76) {
-      return "G6";
-    }
-    else if (position == 77) {
-      return "G7";
-    }
-    else if (position == 78) {
-      return "G8";
-    }
-
-    else if (position == 81) {
-      return "H1";
-    }
-    else if (position == 82) {
-      return "H2";
-    }
-    else if (position == 83) {
-      return "H3";
-    }
-    else if (position == 84) {
-      return "H4";
-    }
-    else if (position == 85) {
-      return "H5";
-    }
-    else if (position == 86) {
-      return "H6";
-    }
-    else if (position == 87) {
-      return "H7";
-    }
-    else if (position == 88) {
-      return "H8";
-    }
-  };
-
-  $("#" + variableIdName() + "").text("xx");
-
-
-
-
-
-
-
+  $("div").click(function() {
+    // if individualMovement === 0, the player is selecting their piece. if the individualMovement === 1 the player is selecting where the piece will go.
+    if (individualMovement === 0) {
+      // takes div id and uses it to find it's corresponding place on the array boardboard
+      startPosition.push($(this).attr('id')[0]);
+      startPosition.push($(this).attr('id')[1]);
+      horizontalStartPosition = startPosition[0];
+      veritcalStartPosition = startPosition[1];
+      // takes player's piece so it can be moved
+      currentPiece.push($(this).html());
+      //declares whether the spot clicked on the board is occupied, and if so what color it is.
+      if (window[horizontalStartPosition][veritcalStartPosition] === 0) {
+        alert("There isn't anything there!");
+        startPosition = [];
+        currentPiece = [];
+      } else if (window[horizontalStartPosition][veritcalStartPosition] === playerColor) {
+        alert("Move your piece");
+        individualMovement += 1;
+      } else if (window[horizontalStartPosition][veritcalStartPosition] !== playerColor) {
+        alert("You can't move pieces that aren't yours");
+        startPosition = [];
+        currentPiece = [];
+      }
+      return false;
+    } else if (individualMovement === 1 && playerColor === redPiece) {
+      // gets div id and uses it to find the ending position on board
+      endingPosition.push($(this).attr('id')[0]);
+      endingPosition.push($(this).attr('id')[1]);
+      horizontalEndingPosition = endingPosition[0];
+      veritcalEndingPosition = endingPosition[1];
+      //makes the div id manipulatable in our function
+      startPositionID = startPosition.join("");
+      endingPositionID = endingPosition.join("");
+      if (horizontalEndingPosition === letters[letters.indexOf(horizontalStartPosition)+1] && parseInt(veritcalEndingPosition) === parseInt(veritcalStartPosition)+1 || horizontalEndingPosition === letters[letters.indexOf(horizontalStartPosition)-1] && parseInt(veritcalEndingPosition) === parseInt(veritcalStartPosition)+1) {
+        if (window[horizontalEndingPosition][veritcalEndingPosition] !== 0) {
+          redScore++;
+          $("span#redScore").empty();
+          $("span#redScore").prepend(redScore);
+        } else {
+        }
+        // moves the pieces on the board.
+        window[horizontalStartPosition][veritcalStartPosition] = emptySpace;
+        window[horizontalEndingPosition][veritcalEndingPosition] = redPiece;
+        $(window[startPositionID]).empty();
+        $(window[endingPositionID]).empty();
+        $(window[endingPositionID]).prepend(currentPiece);
+        // shows a console log of the board
+        console.log(A, B, C, D, E, F, G, H);
+        // resets our function
+        endingPosition = [];
+        startPosition = [];
+        currentPiece = [];
+        //ends this players turn
+        individualMovement -= 1;
+        playerColor += 1;
+      } else {
+        alert("NOT A VALID MOVESET TRY AGAIN");
+        startPosition = [];
+        endingPosition = [];
+        currentPiece = [];
+        individualMovement -= 1;
+      }
+    } else if (individualMovement === 1 && playerColor === blackPiece) {
+      // gets div id and uses it to find the ending position on board
+      endingPosition.push($(this).attr('id')[0]);
+      endingPosition.push($(this).attr('id')[1]);
+      horizontalEndingPosition = endingPosition[0];
+      veritcalEndingPosition = endingPosition[1];
+      //makes the div id manipulatable in our function
+      startPositionID = startPosition.join("");
+      endingPositionID = endingPosition.join("");
+      if (horizontalEndingPosition === letters[letters.indexOf(horizontalStartPosition)+1] && parseInt(veritcalEndingPosition) === parseInt(veritcalStartPosition)-1 || horizontalEndingPosition === letters[letters.indexOf(horizontalStartPosition)-1] && parseInt(veritcalEndingPosition) === parseInt(veritcalStartPosition)-1) {
+        if (window[horizontalEndingPosition][veritcalEndingPosition] !== 0) {
+          blackScore++;
+          $("span#blackScore").empty();
+          $("span#blackScore").prepend(blackScore);
+        } else {
+        }
+        // moves the pieces on the board.
+        window[horizontalStartPosition][veritcalStartPosition] = emptySpace;
+        window[horizontalEndingPosition][veritcalEndingPosition] = blackPiece;
+        $(window[startPositionID]).empty();
+        $(window[endingPositionID]).empty();
+        $(window[endingPositionID]).prepend(currentPiece);
+        // shows a console log of the board
+        console.log(A, B, C, D, E, F, G, H);
+        // resets our function
+        endingPosition = [];
+        startPosition = [];
+        currentPiece = [];
+        //ends this players turn
+        individualMovement -= 1;
+        playerColor -= 1;
+      } else {
+        alert("NOT A VALID MOVESET TRY AGAIN");
+        endingPosition = [];
+        startPosition = [];
+        currentPiece = [];
+        individualMovement -= 1;
+      }
+    }
+   });
 });
-
-
-//column id's
-// $("#A8")  $("#B8")  $("#C8")  $("#D8")  $("#E8")  $("#F8")  $("#G8")  $("#H8")
-// $("#A7")  $("#B7")  $("#C7")  $("#D7")  $("#E7")  $("#F7")  $("#G7")  $("#H7")
-// $("#A6")  $("#B6")  $("#C6")  $("#D6")  $("#E6")  $("#F6")  $("#G6")  $("#H6")
-// $("#A5")  $("#B5")  $("#C5")  $("#D5")  $("#E5")  $("#F5")  $("#G5")  $("#H5")
-// $("#A4")  $("#B4")  $("#C4")  $("#D4")  $("#E4")  $("#F4")  $("#G4")  $("#H4")
-// $("#A3")  $("#B3")  $("#C3")  $("#D3")  $("#E3")  $("#F3")  $("#G3")  $("#H3")
-// $("#A2")  $("#B2")  $("#C2")  $("#D2")  $("#E2")  $("#F2")  $("#G2")  $("#H2")
-// $("#A1")  $("#B1")  $("#C1")  $("#D1")  $("#E1")  $("#F1")  $("#G1")  $("#H1")
-//
-// $("#A1")
-// $("#A2")
-// $("#A3")
-// $("#A4")
-// $("#A5")
-// $("#A6")
-// $("#A7")
-// $("#A8")
-//
-// $("#B1")
-// $("#B2")
-// $("#B3")
-// $("#B4")
-// $("#B5")
-// $("#B6")
-// $("#B7")
-// $("#B8")
-//
-// $("#C1")
-// $("#C2")
-// $("#C3")
-// $("#C4")
-// $("#C5")
-// $("#C6")
-// $("#C7")
-// $("#C8")
-//
-// $("#D1")
-// $("#D2")
-// $("#D3")
-// $("#D4")
-// $("#D5")
-// $("#D6")
-// $("#D7")
-// $("#D8")
-//
-// $("#E1")
-// $("#E2")
-// $("#E3")
-// $("#E4")
-// $("#E5")
-// $("#E6")
-// $("#E7")
-// $("#E8")
-//
-// $("#F1")
-// $("#F2")
-// $("#F3")
-// $("#F4")
-// $("#F5")
-// $("#F6")
-// $("#F7")
-// $("#F8")
-//
-// $("#G1")
-// $("#G2")
-// $("#G3")
-// $("#G4")
-// $("#G5")
-// $("#G6")
-// $("#G7")
-// $("#G8")
-//
-// $("#H1")
-// $("#H2")
-// $("#H3")
-// $("#H4")
-// $("#H5")
-// $("#H6")
-// $("#H7")
-// $("#H8")
